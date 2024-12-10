fabd0148c02e692ee77bcc869e644e227bdeb2f7
Vito Canadi <vito.canadi@gmail.com>
2024-12-10 01:03:45 +0100

    Example of compile-time godot file generation

--- src/Godot/Lang/Example/TH.hs
+++ src/Godot/Lang/Example/TH.hs
@@ -6,12 +6,17 @@
 module Godot.Lang.Example.TH where
 
 import Godot.Lang.Example.Common ()
-import Godot.Lang.Example.CliMsg
+import Godot.Lang.Example.CliMsg ()
 import Godot.Lang.Example.SrvMsg ()
 import Godot.Lang.TH
-import Godot.Lang.Class (ToDC)
+-- import Godot.Lang.Class (toGDScriptExtra)
+-- import Data.Proxy (Proxy(Proxy))
+-- import Language.Haskell.TH
 
 -- Add any addition instances Of ToDC
-instance ToDC (Maybe Dir)
 
-$(allToDCInsts)
+-- | Paste here declaration 'newtype AllToDCInsts = [...type level list of all ToDC instances...]'
+$(qAllToDCInsts)
+
+-- | Example how to run toGDScriptExtra at compiletime to generate ./common.gd file
+-- $(runIO (toGDScriptExtra "." (Proxy @AllToDCInsts)) >> pure [])
--- src/Godot/Lang/TH.hs
+++ src/Godot/Lang/TH.hs
@@ -1,24 +1,29 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
 
 {- | Automatic code generations based on ToDC instances
 -}
 
 module Godot.Lang.TH where
 
-import Language.Haskell.TH
-import Godot.Lang.Class (ToDC)
+import Language.Haskell.TH (Q,Dec(InstanceD, TySynD), Info(ClassI), mkName, reify, runIO , Type(AppT, PromotedConsT, PromotedNilT))
+import Godot.Lang.Class (ToDC, toGDScriptExtra, ToDCsExtra)
 import Data.Maybe (mapMaybe)
+import Data.Proxy
 
--- get a list of instances
-getToDCInstTypes :: Q [Type]
-getToDCInstTypes = do
+-- | Get a list of ToDC instances
+qGetToDCInstTypes :: Q [Type]
+qGetToDCInstTypes = do
   ClassI _ insts <- reify ''ToDC
   let extractType (InstanceD _ _ (AppT _ t) _) = Just t
       extractType _                            = Nothing
   pure $ mapMaybe extractType insts
 
 -- | Declaration: 'newtype AllToDCInsts = [...type level list of all ToDC instances...]'
-allToDCInsts :: Q [Dec]
-allToDCInsts = do
-  types <-  getToDCInstTypes
+qAllToDCInsts :: Q [Dec]
+qAllToDCInsts = do
+  types <-  qGetToDCInstTypes
   pure [TySynD (mkName "AllToDCInsts") [] $ foldr (AppT . AppT PromotedConsT) PromotedNilT types]
+
+qRunToGDScriptExtra :: forall as. ToDCsExtra as => Proxy as -> Q [Dec]
+qRunToGDScriptExtra _ = runIO (toGDScriptExtra "." (Proxy @as)) >> pure []
